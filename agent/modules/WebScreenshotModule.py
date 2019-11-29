3fa3458c46a19136254ba830a714ce9b13ae4669
Wocanilo <25706428+Wocanilo@users.noreply.github.com>
2019-11-29 17:41:34 +0100

    Fixed Upload of files

--- agent/modules/WebScreenshotModule.py
+++ agent/modules/WebScreenshotModule.py
@@ -28,8 +28,7 @@ class Module(BaseModule):
                 # Send picture to server
                 self.callback.debug("----------------------------JOB {} update----------------------------\n{}".format(self.params["jobId"], WebScreenshot(weburl.serviceId, weburl.path, None)))
                 if(not self.params["DISABLE_MASTER_SERVER"]):
-                    r = requests.post("https://{}/job/screenshotUpload".format(self.params["MASTER_DOMAIN"]), files={"picture": open(screenshot.path, "rb"), "service_id": weburl.serviceId,
-                    "path": weburl.path, "jobId": self.params["jobId"]})
+                    r = requests.post("https://{}/api/job/screenshotUpload/{}".format(self.params["MASTER_DOMAIN"], weburl.serviceId), files={"picture": open(screenshot.path, "rb")})
                     self.callback.debug("----------------------------Response from master for JOB {} UPDATE----------------------------\n{}".format(self.params["jobId"], r.text))
                     
                 # Delete picture
